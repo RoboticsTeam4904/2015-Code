d672e8ac922b19d0f0f1dfa36cdb95abc80cbe93
Carter Turnbaugh <carturn@nuevaschool.org>
2015-01-21 16:04:14 -0800

    Added comments on Xbox interface.

--- 2015-Code/src/org/usfirst/frc/team4904/robot/Xbox.java
+++ 2015-Code/src/org/usfirst/frc/team4904/robot/Xbox.java
@@ -21,16 +21,16 @@ public class Xbox extends Joystick{
 		super(port);
 		// TODO Auto-generated constructor stub
 		
-		x1 = new JoystickButton(this, 1);
-		x2 = new JoystickButton(this, 2);
-		x3 = new JoystickButton(this, 3);
-		x4 = new JoystickButton(this, 4);
-		x5 = new JoystickButton(this, 5);
-		x6 = new JoystickButton(this, 6);
-		x7 = new JoystickButton(this, 7);
-		x8 = new JoystickButton(this, 8);
-		x9 = new JoystickButton(this, 9);
-		x10 = new JoystickButton(this, 10);
+		x1 = new JoystickButton(this, 1); // A button
+		x2 = new JoystickButton(this, 2); // B button
+		x3 = new JoystickButton(this, 3); // X button
+		x4 = new JoystickButton(this, 4); // Y button
+		x5 = new JoystickButton(this, 5); // Left bumper
+		x6 = new JoystickButton(this, 6); // Right bumper
+		x7 = new JoystickButton(this, 7); // Back button
+		x8 = new JoystickButton(this, 8); // Start button
+		x9 = new JoystickButton(this, 9); // Left stick
+		x10 = new JoystickButton(this, 10); // Right stick
 	}
 	
 	
