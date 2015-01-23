da9f269755a8f7bfd5fdaf3df119bd54f28880ec
leijurv <leijurv@gmail.com>
2015-01-22 16:22:41 -0800

    Fixed things

--- 2015-Code/src/org/usfirst/frc/team4904/robot/Grabber.java
+++ 2015-Code/src/org/usfirst/frc/team4904/robot/Grabber.java
@@ -6,9 +6,6 @@ public class Grabber extends Talon {
 	
 	static final double MAX_WIDTH = 100;
 
-	public static final int MODE_THIN_TOTE = 0;
-	public static final int MODE_WIDE_TOTE = 1;
-	public static final int MODE_CAN = 2;
 
 	public static final double THIN_TOTE_WIDTH = 0;
 	public static final double WIDE_TOTE_WIDTH = 0;
@@ -23,9 +20,9 @@ public class Grabber extends Talon {
 	}
 	
 	public void setWidth(int mode){
-		if (mode == MODE_THIN_TOTE) moveToWidth(THIN_TOTE_WIDTH);
-		else if (mode == MODE_WIDE_TOTE) moveToWidth(WIDE_TOTE_WIDTH);
-		else if (mode == MODE_CAN) moveToWidth(CAN_WIDTH);
+		if (mode == Operator.MODE_THIN_TOTE) moveToWidth(THIN_TOTE_WIDTH);
+		else if (mode == Operator.MODE_WIDE_TOTE) moveToWidth(WIDE_TOTE_WIDTH);
+		else if (mode == Operator.MODE_CAN) moveToWidth(CAN_WIDTH);
 	}
 	
 	public void move(double speed){
