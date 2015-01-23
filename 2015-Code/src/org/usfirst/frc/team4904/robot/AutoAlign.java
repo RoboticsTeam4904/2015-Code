c4c39a2b3e340831f78300a3771b8d438c29027c
leijurv <leijurv@gmail.com>
2015-01-22 16:21:50 -0800

    Fixed things

--- 2015-Code/src/org/usfirst/frc/team4904/robot/AutoAlign.java
+++ 2015-Code/src/org/usfirst/frc/team4904/robot/AutoAlign.java
@@ -19,20 +19,20 @@ public class AutoAlign {
 		
 		boolean wide = false;
 		
-		if (wide) grabber.setWidth(Grabber.MODE_WIDE_TOTE); // if we are picking up a wide tote, set the grabber to the wide tote width
-		else grabber.setWidth(Grabber.MODE_THIN_TOTE); // if we are picking up a thin tote, set the grabber to the thin tote width. If you are reading the end of this comment, you must be looking for significantly more information.
+		if (wide) grabber.setWidth(Operator.MODE_WIDE_TOTE); // if we are picking up a wide tote, set the grabber to the wide tote width
+		else grabber.setWidth(Operator.MODE_THIN_TOTE); // if we are picking up a thin tote, set the grabber to the thin tote width. If you are reading the end of this comment, you must be looking for significantly more information.
 		}
 	
 	public void canGrab(){
 		
 		
-		grabber.setWidth(Grabber.MODE_CAN);
+		grabber.setWidth(Operator.MODE_CAN);
 	}
 	
 	public void toteRelease(){
 		
 		
-		grabber.setWidth(Grabber.MODE_WIDE_TOTE+1); // set 
+		grabber.setWidth(Operator.MODE_WIDE_TOTE+1); // set 
 	}
 	
 	public void canRelease(){
