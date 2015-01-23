fb2bd7acd2871a73a29017ffe3e9b978a7001861
Carter Turnbaugh <carturn@nuevaschool.org>
2015-01-22 16:20:45 -0800

    Added comments. Removed some redundant variables.

--- 2015-Code/src/org/usfirst/frc/team4904/robot/AutoAlign.java
+++ 2015-Code/src/org/usfirst/frc/team4904/robot/AutoAlign.java
@@ -19,24 +19,26 @@ public class AutoAlign {
 		
 		boolean wide = false;
 		
-		if (wide) grabber.setWidth(grabber.MODE_WIDE_TOTE);
-		else grabber.setWidth(grabber.MODE_THIN_TOTE);
+		if (wide) grabber.setWidth(Grabber.MODE_WIDE_TOTE); // if we are picking up a wide tote, set the grabber to the wide tote width
+		else grabber.setWidth(Grabber.MODE_THIN_TOTE); // if we are picking up a thin tote, set the grabber to the thin tote width. If you are reading the end of this comment, you must be looking for significantly more information.
 		}
 	
 	public void canGrab(){
 		
 		
-		grabber.setWidth(grabber.MODE_CAN);
+		grabber.setWidth(Grabber.MODE_CAN);
 	}
 	
 	public void toteRelease(){
 		
 		
-		grabber.setWidth(grabber.MODE_WIDE_TOTE+1);
+		grabber.setWidth(Grabber.MODE_WIDE_TOTE+1); // set 
 	}
 	
 	public void canRelease(){
 		
+		
+		grabber.setWidth(Operator.MODE_CAN+1);
 	}
 	
 }
