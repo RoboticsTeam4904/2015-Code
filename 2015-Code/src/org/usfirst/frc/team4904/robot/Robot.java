b8f4e2c5c871829134dbfcbb700473bf2a5762d0
leijurv <leijurv@gmail.com>
2015-01-22 16:47:20 -0800

    Removed unused import

--- 2015-Code/src/org/usfirst/frc/team4904/robot/Robot.java
+++ 2015-Code/src/org/usfirst/frc/team4904/robot/Robot.java
@@ -1,7 +1,6 @@
 package org.usfirst.frc.team4904.robot;
 
 import edu.wpi.first.wpilibj.SampleRobot;
-import edu.wpi.first.wpilibj.Talon;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.VictorSP;
 
