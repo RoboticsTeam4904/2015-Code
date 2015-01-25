5d4be5d6b5cc11469ed41dd2c77a1d9dd8b71668
leijurv <leijurv@gmail.com>
2015-01-25 12:22:56 -0800

    Changed something

--- 2015-Code/src/org/usfirst/frc/team4904/robot/Robot.java
+++ 2015-Code/src/org/usfirst/frc/team4904/robot/Robot.java
@@ -1,6 +1,5 @@
 package org.usfirst.frc.team4904.robot;
 
-
 import org.usfirst.frc.team4904.robot.driver.DriverAutonomous;
 import org.usfirst.frc.team4904.robot.driver.DriverNathan;
 import org.usfirst.frc.team4904.robot.input.IMU;
@@ -13,6 +12,7 @@ import org.usfirst.frc.team4904.robot.operator.OperatorGriffin;
 import org.usfirst.frc.team4904.robot.output.Grabber;
 import org.usfirst.frc.team4904.robot.output.Mecanum;
 import org.usfirst.frc.team4904.robot.output.Winch;
+
 import edu.wpi.first.wpilibj.SampleRobot;
 import edu.wpi.first.wpilibj.SpeedController;
 import edu.wpi.first.wpilibj.Timer;
@@ -29,11 +29,14 @@ public class Robot extends SampleRobot {
 	private static final int BACK_RIGHT_WHEEL_PORT = 3;
 	private static final int WINCH_PORT = 4;
 	private static final int GRABBER_PORT = 5;
+
 	private final LogitechJoystick stick; // the X3D Extreme3DPro Logitech joystick (right hand) - operator
 	private final XboxController xboxController; // the Xbox 360 controller - driver
+
 	private final UDAR udar; // the UDAR (ultrasonic detection and ranging)
 	private final IMU imu;
 	private final LIDAR lidar;
+
 	// movement controllers
 	private final Winch winch; // the Winch class takes care of moving to specific heights
 	private final Grabber grabber; // the grabber class takes care of openning and closing the grabber
@@ -49,18 +52,21 @@ public class Robot extends SampleRobot {
 	private final Driver autonomousDriver;
 	private final Operator autonomousOperator;
 	private final AutonomousController controller;
+
 	private final AutoAlign align; // the AutoAlign class contains code to align the robot with totes and cans
+
 	// Update system
 	private final double fastUpdatePeriod = 0.005; // update every 0.005 seconds/5 milliseconds (200Hz)
 	private final double slowUpdatePeriod = 0.5; // update every 0.005 seconds/5 milliseconds (200Hz)
-	
+
 	private enum RobotState {
 		DISABLED, OPERATOR, AUTONOMOUS
 	}
-	
+
 	public Robot() {
 		System.out.println("*** INITIALIZING ROBOT ***");
-		// Initialize movement controllers
+
+		//Initialize movement controllers
 		winch = new Winch(WINCH_PORT); // Initialize Winch control
 		grabber = new Grabber(GRABBER_PORT); // Initialize Grabber control -- only autoalign has access to this, by design
 		// Initialize motor controllers with default ports
@@ -68,14 +74,18 @@ public class Robot extends SampleRobot {
 		frontRightWheel = new VictorSP(FRONT_RIGHT_WHEEL_PORT);
 		backLeftWheel = new VictorSP(BACK_LEFT_WHEEL_PORT);
 		backRightWheel = new VictorSP(BACK_RIGHT_WHEEL_PORT);
-		mecanumDrive = new Mecanum(frontLeftWheel, frontRightWheel, backLeftWheel, backRightWheel); // Initialize Mecanum control
+		mecanumDrive = new Mecanum(frontLeftWheel, frontRightWheel,
+				backLeftWheel, backRightWheel); // Initialize Mecanum control
+
 		// Initialize joysticks (numbers correspond to value set by driver station)
 		stick = new LogitechJoystick(JOYSTICK_PORT);
 		xboxController = new XboxController(CONTROLLER_PORT);
-		// Initialize sensors
+
+		//Initialize sensors
 		imu = new IMU(); // Initialize IMU
 		udar = new UDAR(); // Initialize UDAR
 		lidar = new LIDAR(); // Initalize LIDAR
+
 		align = new AutoAlign(mecanumDrive, udar, lidar, imu, grabber); // Initialize AutoAlign system
 		humanOperator = new OperatorGriffin(stick, winch, align);
 		humanDriver = new DriverNathan(mecanumDrive, xboxController, align);
@@ -83,7 +93,7 @@ public class Robot extends SampleRobot {
 		autonomousOperator = new OperatorAutonomous(winch, align, controller);
 		autonomousDriver = new DriverAutonomous(mecanumDrive, controller, align);
 	}
-	
+
 	public void disabled() {
 		System.out.println("*** DISABLED ***");
 		while (isDisabled()) {
@@ -91,7 +101,7 @@ public class Robot extends SampleRobot {
 			Timer.delay(0.01);
 		}
 	}
-	
+
 	private void disableMotors() {
 		winch.set(0);
 		grabber.set(0);
@@ -100,31 +110,35 @@ public class Robot extends SampleRobot {
 		backLeftWheel.set(0);
 		backRightWheel.set(0);
 	}
-	
+
 	public void autonomous() {
 		System.out.println("*** AUTONOMOUS ***");
 		operator = autonomousOperator;
 		driver = autonomousDriver;
 		RobotState state = RobotState.AUTONOMOUS;
-		new Updater(state, new Updatable[] {controller, align}, slowUpdatePeriod).start(); // Controller and align are potentially slower
-		new Updater(state, new Updatable[] {imu, driver, operator, mecanumDrive, lidar}, fastUpdatePeriod).start(); // These should have fast updates
+		new Updater(state, new Updatable[] { controller, align },
+				slowUpdatePeriod).start(); // Controller and align are potentially slower
+		new Updater(state, new Updatable[] { imu, driver, operator,
+				mecanumDrive, lidar }, fastUpdatePeriod).start(); // These should have fast updates
 		while (getRobotState() == state) {
 			Timer.delay(0.01);
 		}
 	}
-	
+
 	public void operatorControl() {
 		System.out.println("*** TELEOPERATED ***");
 		operator = humanOperator;
 		driver = humanDriver;
 		RobotState state = RobotState.OPERATOR;
-		new Updater(state, new Updatable[] {controller, align}, slowUpdatePeriod).start(); // Controller and align are potentially slower
-		new Updater(state, new Updatable[] {imu, driver, operator, mecanumDrive, lidar}, fastUpdatePeriod).start(); // These should have fast updates
+		new Updater(state, new Updatable[] { controller, align },
+				slowUpdatePeriod).start(); // Controller and align are potentially slower
+		new Updater(state, new Updatable[] { imu, driver, operator,
+				mecanumDrive, lidar }, fastUpdatePeriod).start(); // These should have fast updates
 		while (getRobotState() == state) {
 			Timer.delay(0.01);
 		}
 	}
-	
+
 	private RobotState getRobotState() {
 		if (isDisabled()) {
 			return RobotState.DISABLED;
@@ -137,22 +151,23 @@ public class Robot extends SampleRobot {
 		}
 		return RobotState.DISABLED;
 	}
-	
+
 	private static double time() {
 		return ((double) System.currentTimeMillis()) / 1000;
 	}
-	
+
 	private class Updater extends Thread { // Function to update automatically in a new thread
 		private final RobotState robotState;
 		private final Updatable[] toUpdate;
 		private final double updateSpeed;
-		
-		public Updater(RobotState state, Updatable[] toUpdate, double updateSpeed) {
+
+		public Updater(RobotState state, Updatable[] toUpdate,
+				double updateSpeed) {
 			this.robotState = state;
 			this.toUpdate = toUpdate;
 			this.updateSpeed = updateSpeed;
 		}
-		
+
 		public void run() {
 			double desiredTime = time() + updateSpeed; // Sync with clock to ensure that update interval is consistant regardless of how long each update takes
 			while (getRobotState() == robotState) {
