3711e4e8ad08413517078eec4ac913e5c2c7f870
Carter Turnbaugh <carturn@nuevaschool.org>
2015-01-25 12:27:21 -0800

    Using new SuperButton with Operators.

--- 2015-Code/src/org/usfirst/frc/team4904/robot/operator/OperatorGriffin.java
+++ 2015-Code/src/org/usfirst/frc/team4904/robot/operator/OperatorGriffin.java
@@ -15,17 +15,17 @@ public class OperatorGriffin extends Operator {
 	}
 	
 	public synchronized void update() {
-		if (stick.x2.get()) adjust(stick.getY()); // When button 2 is pressed, adjust the winch
-		if (stick.x4.get()) raise(1); // When button 4 is pressed, raise the winch one level
-		if (stick.x3.get()) lower(1); // When button 3 is pressed, lower the winch one level
-		if (stick.x6.get()) raise(10); // When button 6 is pressed, raise the winch all the way
-		if (stick.x5.get()) lower(10); // When button 5 is pressed, lower the winch all the way
-		if (stick.x12.get()) grab(MODE_THIN_TOTE); // When button 12 is pressed, grab a thin tote
-		if (stick.x10.get()) grab(MODE_WIDE_TOTE); // When button 10 is pressed, grab a wide tote
-		if (stick.x8.get()) grab(MODE_CAN); // When button 8 is pressed, grab a can
-		if (stick.x11.get()) release(); // When button 11 is pressed, release a thin tote
-		if (stick.x9.get()) release(); // When button 9 is pressed, release a wide tote
-		if (stick.x7.get()) release(); // When button 7 is pressed, release a can
+		if (stick.buttons[1].get()) adjust(stick.getY()); // When button 2 is pressed, adjust the winch
+		if (stick.buttons[3].get()) raise(1); // When button 4 is pressed, raise the winch one level
+		if (stick.buttons[2].get()) lower(1); // When button 3 is pressed, lower the winch one level
+		if (stick.buttons[5].get()) raise(10); // When button 6 is pressed, raise the winch all the way
+		if (stick.buttons[4].get()) lower(10); // When button 5 is pressed, lower the winch all the way
+		if (stick.buttons[11].get()) grab(MODE_THIN_TOTE); // When button 12 is pressed, grab a thin tote
+		if (stick.buttons[9].get()) grab(MODE_WIDE_TOTE); // When button 10 is pressed, grab a wide tote
+		if (stick.buttons[7].get()) grab(MODE_CAN); // When button 8 is pressed, grab a can
+		if (stick.buttons[10].get()) release(); // When button 11 is pressed, release a thin tote
+		if (stick.buttons[8].get()) release(); // When button 9 is pressed, release a wide tote
+		if (stick.buttons[6].get()) release(); // When button 7 is pressed, release a can
 		updateWinch();
 	}
 }
