a92f3601de3c43d7bf97f7cea654e73601ec7812
Carter Turnbaugh <carturn@nuevaschool.org>
2015-01-25 11:12:25 -0800

    Added debouncing for Xbox buttons.

--- 2015-Code/src/org/usfirst/frc/team4904/robot/input/XboxController.java
+++ 2015-Code/src/org/usfirst/frc/team4904/robot/input/XboxController.java
@@ -7,33 +7,51 @@ import edu.wpi.first.wpilibj.buttons.JoystickButton;
 
 public class XboxController extends Joystick{
 	
-	public Button aButton;
-	public Button bButton;
-	public Button xButton;
-	public Button yButton;
-	public Button leftBumper;
-	public Button rightBumper;
-	public Button backButton;
-	public Button startButton;
-	public Button leftStick;
-	public Button rightStick;
+	public final int A_BUTTON = 1;
+	public final int B_BUTTON = 2;
+	public final int X_BUTTON = 3;
+	public final int Y_BUTTON = 4;
+	public final int LEFT_BUMPER = 5;
+	public final int RIGHT_BUMPER = 6;
+	public final int BACK_BUTTON = 7;
+	public final int START_BUTTON = 8;
+	public final int LEFT_STICK = 9;
+	public final int RIGHT_STICK = 10;
+	
+	private Button[] buttons = new Button[10];
+	private boolean[] pressed = new boolean[10];
 	
 	public XboxController(int port) {
 		super(port);
-		aButton = new JoystickButton(this, 1); // A button
-		bButton = new JoystickButton(this, 2); // B button
-		xButton = new JoystickButton(this, 3); // X button
-		yButton = new JoystickButton(this, 4); // Y button
-		leftBumper = new JoystickButton(this, 5); // Left bumper
-		rightBumper = new JoystickButton(this, 6); // Right bumper
-		backButton = new JoystickButton(this, 7); // Back button
-		startButton = new JoystickButton(this, 8); // Start button
-		leftStick = new JoystickButton(this, 9); // Left stick
-		rightStick = new JoystickButton(this, 10); // Right stick
+		buttons[0] = new JoystickButton(this, A_BUTTON); // A button
+		buttons[1] = new JoystickButton(this, B_BUTTON); // B button
+		buttons[2] = new JoystickButton(this, X_BUTTON); // X button
+		buttons[3] = new JoystickButton(this, Y_BUTTON); // Y button
+		buttons[4] = new JoystickButton(this, LEFT_BUMPER); // Left bumper
+		buttons[5] = new JoystickButton(this, RIGHT_BUMPER); // Right bumper
+		buttons[6] = new JoystickButton(this, BACK_BUTTON); // Back button
+		buttons[7] = new JoystickButton(this, START_BUTTON); // Start button
+		buttons[8] = new JoystickButton(this, LEFT_STICK); // Left stick
+		buttons[9] = new JoystickButton(this, RIGHT_STICK); // Right stick
+		for (int i = 0; i < 10; i++) {
+			pressed[i] = false;
+		}
 	}
 	
-	public boolean get(){
+	public boolean get(int button){
+		boolean button_val = false;
 		
-		return false;
+		button_val = buttons[button-1].get();
+		if (pressed[button - 1] && button_val) {
+			button_val = false;
+		}
+		else if (pressed[button - 1] && !button_val) {
+			pressed[button - 1] = false;
+		}
+		else if (!pressed[button - 1] && button_val) {
+			pressed[button - 1] = true;
+		}
+		
+		return button_val;
 	}
 }
