d21b3ac9377ddfb4c2608edd978a607f2aa6e891
Team4904Classmate <botprovoking@nuevaschool.org>
2015-01-30 08:52:36 -0800

    cats

--- 2015-Code/src/org/usfirst/frc/team4904/robot/driver/DriverNathan.java
+++ 2015-Code/src/org/usfirst/frc/team4904/robot/driver/DriverNathan.java
@@ -8,18 +8,18 @@ import org.usfirst.frc.team4904.robot.output.Mecanum;
 
 public class DriverNathan extends Driver {
 	private final XboxController xboxController;
-	
+
 	public DriverNathan(Mecanum mecanumDrive, XboxController xboxController, AutoAlign align) {
 		super(mecanumDrive, align);
 		this.xboxController = xboxController;
 	}
-	
+
 	public synchronized void update() {
 		// System.out.println("X: " + xboxController.getX() + " Y: " + xboxController.getY());
 		double angle = Math.atan2(-xboxController.getValue(XboxController.X_STICK), -xboxController.getValue(XboxController.Y_STICK));
 		double speed = Math.sqrt(xboxController.getValue(XboxController.Y_STICK) * xboxController.getValue(XboxController.Y_STICK) + xboxController.getValue(XboxController.X_STICK) * xboxController.getValue(XboxController.X_STICK)); // TODO Fix because that is not how trig works
 		setMovement(speed, angle);
-		double turnSpeed = xboxController.getValue(XboxController.TWIST_STICK);
+		double turnSpeed = xboxController.getValue(XboxController.TWIST_STICK) / 5;// Turns way too fast otherwise
 		setTurn(turnSpeed); // Actually do the turning
 	}
 }
