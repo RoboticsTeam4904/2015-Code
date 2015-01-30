09c33609f8baaa7efc8a33ef119b74a7fd2bd526
Team4904Classmate <botprovoking@nuevaschool.org>
2015-01-30 08:46:45 -0800

    Changed things

--- 2015-Code/src/org/usfirst/frc/team4904/robot/Robot.java
+++ 2015-Code/src/org/usfirst/frc/team4904/robot/Robot.java
@@ -55,11 +55,11 @@ public class Robot extends SampleRobot {
 	// Update system
 	private final double fastUpdatePeriod = 0.005; // update every 0.005 seconds/5 milliseconds (200Hz)
 	private final double slowUpdatePeriod = 0.5; // update every 0.005 seconds/5 milliseconds (200Hz)
-	
+
 	private enum RobotState {
 		DISABLED, OPERATOR, AUTONOMOUS
 	}
-	
+
 	public Robot() {
 		System.out.println("*** INITIALIZING ROBOT ***");
 		// Initialize movement controllers
@@ -87,7 +87,7 @@ public class Robot extends SampleRobot {
 		autonomousOperator = new OperatorAutonomous(winch, align, controller);
 		autonomousDriver = new DriverAutonomous(mecanumDrive, controller, align);
 	}
-	
+
 	public void disabled() {
 		System.out.println("*** DISABLED ***");
 		while (isDisabled()) {
@@ -95,7 +95,7 @@ public class Robot extends SampleRobot {
 			Timer.delay(0.01);
 		}
 	}
-	
+
 	private void disableMotors() {
 		winch.set(0);
 		grabber.set(0);
@@ -105,7 +105,7 @@ public class Robot extends SampleRobot {
 		backRightWheel.set(0);
 		lidar.motor.set(0);
 	}
-	
+
 	public void autonomous() {
 		System.out.println("*** AUTONOMOUS ***");
 		operator = autonomousOperator;
@@ -117,7 +117,7 @@ public class Robot extends SampleRobot {
 			Timer.delay(0.01);
 		}
 	}
-	
+
 	public void operatorControl() {
 		System.out.println("*** TELEOPERATED ***");
 		operator = humanOperator;
@@ -129,7 +129,7 @@ public class Robot extends SampleRobot {
 			Timer.delay(0.01);
 		}
 	}
-	
+
 	private RobotState getRobotState() {
 		if (isDisabled()) {
 			return RobotState.DISABLED;
@@ -142,22 +142,22 @@ public class Robot extends SampleRobot {
 		}
 		return RobotState.DISABLED;
 	}
-	
+
 	private static double time() {
 		return (double) System.currentTimeMillis() / 1000;
 	}
-	
+
 	private class Updater extends Thread { // Function to update automatically in a new thread
 		private final RobotState robotState;
 		private final Updatable[] toUpdate;
 		private final double updateSpeed;
-		
+
 		public Updater(RobotState state, Updatable[] toUpdate, double updateSpeed) {
 			robotState = state;
 			this.toUpdate = toUpdate;
 			this.updateSpeed = updateSpeed;
 		}
-		
+
 		public void run() {
 			double desiredTime = time() + updateSpeed; // Sync with clock to ensure that update interval is consistent regardless of how long each update takes
 			while (getRobotState() == robotState) {
--- 2015-Code/src/org/usfirst/frc/team4904/robot/input/IMU.java
+++ 2015-Code/src/org/usfirst/frc/team4904/robot/input/IMU.java
@@ -6,33 +6,33 @@ import org.usfirst.frc.team4904.robot.Updatable;
 public class IMU implements Updatable {
 	private MPU9150 mpu9150;
 	private int[] rawData = new int[10];
-
+	
 	public IMU() {
-		this.mpu9150 = new MPU9150();
-		this.mpu9150.init();
-		this.zero();
+		// this.mpu9150 = new MPU9150();
+		// this.mpu9150.init();
+		zero();
 	}
-
+	
 	public float getAngle() {
 		// TODO return current robot angle relative to zero point (0 - 360)
 		return 0F;
 	}
-
+	
 	public void zero() {
 		// TODO set current orientation as "forward"
-		this.update();
+		update();
 	}
-
+	
 	public synchronized void update() {
-		this.readData();
-		this.updateKalman();
+		// readData();
+		// updateKalman();
 	}
-
+	
 	private void readData() {
 		// TODO only read data if enough data is available, otherwise return so
 		// that this function is always fast
-		this.rawData = this.mpu9150.read();
+		// rawData = mpu9150.read();
 	}
-
+	
 	private void updateKalman() {}
 }
