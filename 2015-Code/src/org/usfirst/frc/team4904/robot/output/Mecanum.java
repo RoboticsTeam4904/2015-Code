7c943f098b76221b56fee3763e51684b8c5b97be
c0d3rman <yonilern@gmail.com>
2015-01-30 12:43:29 -0800

    Added Mecanum Absolute Move

--- 2015-Code/src/org/usfirst/frc/team4904/robot/output/Mecanum.java
+++ 2015-Code/src/org/usfirst/frc/team4904/robot/output/Mecanum.java
@@ -16,6 +16,7 @@ package org.usfirst.frc.team4904.robot.output;
  * Fixed Turn Function - S
  */
 import org.usfirst.frc.team4904.robot.Updatable;
+import org.usfirst.frc.team4904.robot.input.IMU;
 import edu.wpi.first.wpilibj.SpeedController;
 
 public class Mecanum implements Updatable {
@@ -26,23 +27,33 @@ public class Mecanum implements Updatable {
 	private volatile double desiredSpeed;
 	private volatile double desiredAngle;
 	private volatile double desiredTurnSpeed;
+	private volatile boolean absolute;
+	private final IMU imu;
 
-	public Mecanum(SpeedController frontLeftWheel, SpeedController frontRightWheel, SpeedController backLeftWheel, SpeedController backRightWheel) {
+	public Mecanum(SpeedController frontLeftWheel, SpeedController frontRightWheel, SpeedController backLeftWheel, SpeedController backRightWheel, IMU imu) {
 		// Initialize motor controllers with default ports
 		this.frontLeftWheel = frontLeftWheel;
 		this.frontRightWheel = frontRightWheel;
 		this.backLeftWheel = backLeftWheel;
 		this.backRightWheel = backRightWheel;
+		this.imu = imu;
 	}
 
-	private void move(double desiredSpeed, double desiredAngle, double turnSpeed) {
+	private void move(double desiredSpeed, double desiredAngle, double turnSpeed, boolean absolute) {
 		// @param desiredSpeed double between 0 and 1 specifying wanted movement speed
 		// @param desiredAngle double between 0 and 2pi specifying wanted movement angle in radians
 		// @param turnSpeed double between 0 and 1 specifying rotational speed
-		double frontLeft = desiredSpeed * Math.sin(desiredAngle + Math.PI / 4) + turnSpeed;
-		double frontRight = desiredSpeed * Math.cos(desiredAngle + Math.PI / 4) - turnSpeed;
-		double backLeft = desiredSpeed * Math.cos(desiredAngle + Math.PI / 4) + turnSpeed;
-		double backRight = desiredSpeed * Math.sin(desiredAngle + Math.PI / 4) - turnSpeed;
+		// @param absolute boolean specifying whether to move relative to the robot or absolutely (relative to the compass)
+		double workingAngle;
+		if (absolute) {
+			workingAngle = (desiredAngle - imu.getAngle()) % (Math.PI * 2);
+		} else {
+			workingAngle = desiredAngle;
+		}
+		double frontLeft = desiredSpeed * Math.sin(workingAngle + Math.PI / 4) + turnSpeed;
+		double frontRight = desiredSpeed * Math.cos(workingAngle + Math.PI / 4) - turnSpeed;
+		double backLeft = desiredSpeed * Math.cos(workingAngle + Math.PI / 4) + turnSpeed;
+		double backRight = desiredSpeed * Math.sin(workingAngle + Math.PI / 4) - turnSpeed;
 		double scaleFactor = Math.max(Math.max(Math.max(Math.abs(frontLeft), Math.abs(frontRight)), Math.abs(backLeft)), Math.abs(backRight));
 		if (scaleFactor < 1) {
 			scaleFactor = 1;
@@ -59,7 +70,7 @@ public class Mecanum implements Updatable {
 
 	public synchronized void update() {
 		// System.out.println("Desired speed: " + desiredTurnSpeed + "\tDesired angle: " + desiredAngle + "\tDisired speed: " + desiredSpeed);
-		move(desiredSpeed, desiredAngle, desiredTurnSpeed);// This system allows for different updating times and rates
+		move(desiredSpeed, desiredAngle, desiredTurnSpeed, absolute); // This system allows for different updating times and rates
 	}
 
 	public void setDesiredSpeedDirection(double desiredSpeed, double desiredAngle) {
@@ -67,7 +78,11 @@ public class Mecanum implements Updatable {
 		this.desiredSpeed = desiredSpeed;
 	}
 
-	public void setDesiredTurnSpeed(double turnSpeed) {
-		desiredTurnSpeed = turnSpeed;
+	public void setDesiredTurnSpeed(double desiredTurnSpeed) {
+		this.desiredTurnSpeed = desiredTurnSpeed;
+	}
+	
+	public void setAbsolute(boolean absolute) {
+		this.absolute = absolute;
 	}
 }
