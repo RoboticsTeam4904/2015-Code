3cdd54ed72d09abd2dd233033bde0c59c157eff8
leijurv <leijurv@gmail.com>
2015-02-02 09:04:45 -0800

    Some formatting

--- 2015-Code/src/org/usfirst/frc/team4904/robot/output/Winch.java
+++ 2015-Code/src/org/usfirst/frc/team4904/robot/output/Winch.java
@@ -5,18 +5,16 @@ import edu.wpi.first.wpilibj.Talon;
 
 public class Winch extends Talon {
 	private static transient final int MAX_HEIGHT = 10;
-	
+
 	public Winch(int channel) {
 		super(channel);
 	}
-	private void set(double speed){
-		
-	}
+
 	public void setHeight(int height) { // Set winch to specific height
 		if (height > MAX_HEIGHT) height = MAX_HEIGHT;
 		else if (height < 0) height = 0;
 	}
-	
+
 	public void move(double speed) { // Move winch at speed
 		super.set(speed); // Actually do the moving
 	}
