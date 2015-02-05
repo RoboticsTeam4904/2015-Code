6541ecd6003edb5a6c07e154c7a222d576313fc5
leijurv <leijurv@gmail.com>
2015-02-05 14:24:07 -0800

    Started acceleration dampening

--- 2015-Code/src/org/usfirst/frc/team4904/robot/output/Mecanum.java
+++ 2015-Code/src/org/usfirst/frc/team4904/robot/output/Mecanum.java
@@ -24,12 +24,17 @@ public class Mecanum implements Updatable {
 	private final SpeedController frontRightWheel;
 	private final SpeedController backLeftWheel;
 	private final SpeedController backRightWheel;
-	private volatile double desiredSpeed;
-	private volatile double desiredAngle;
-	private volatile double desiredTurnSpeed;
+	private volatile double currentSpeed;
+	private volatile double currentAngle;
+	private volatile double currentTurnSpeed;
+	private volatile double previousSpeed;
+	private volatile double previousAngle;
+	private volatile double previousTurnSpeed;
+	private volatile double previousTime;
 	private volatile boolean absolute;
+	public static final double maxAccel = 2;// Maximum motor output change per second
 	private final IMU imu;
-
+	
 	public Mecanum(SpeedController frontLeftWheel, SpeedController frontRightWheel, SpeedController backLeftWheel, SpeedController backRightWheel, IMU imu) {
 		// Initialize motor controllers with default ports
 		this.frontLeftWheel = frontLeftWheel;
@@ -38,7 +43,7 @@ public class Mecanum implements Updatable {
 		this.backRightWheel = backRightWheel;
 		this.imu = imu;
 	}
-
+	
 	private void move(double desiredSpeed, double desiredAngle, double turnSpeed, boolean absolute) {
 		// @param desiredSpeed double between 0 and 1 specifying wanted movement speed
 		// @param desiredAngle double between 0 and 2pi specifying wanted movement angle in radians
@@ -68,21 +73,53 @@ public class Mecanum implements Updatable {
 		// System.out.println("Frontright" + (frontRight / scaleFactor));
 	}
 
+	public static double time() {
+		return (double) System.currentTimeMillis() / 1000;
+	}
+
 	public synchronized void update() {
+		double currentTime = time();
+		double timeDifference = currentTime - previousTime;
+		double maxChange = maxAccel * timeDifference;
+		double newSpeed = adjust(previousSpeed, currentSpeed, maxChange);
+		double newAngle = adjust(previousAngle, currentAngle, maxChange);
+		double newTurnSpeed = adjust(previousTurnSpeed, currentTurnSpeed, maxChange);
+		previousSpeed = currentSpeed;
+		previousAngle = currentAngle;
+		previousTurnSpeed = currentTurnSpeed;
+		currentSpeed = newSpeed;
+		currentAngle = newAngle;
+		currentTurnSpeed = newTurnSpeed;
 		// TODO SPEED CONTROLLING GOES HERE
 		// System.out.println("Desired speed: " + desiredTurnSpeed + "\tDesired angle: " + desiredAngle + "\tDisired speed: " + desiredSpeed);
-		move(desiredSpeed, desiredAngle, desiredTurnSpeed, absolute); // This system allows for different updating times and rates
-	}
-
-	public void setDesiredSpeedDirection(double desiredSpeed, double desiredAngle) {
-		this.desiredAngle = desiredAngle;
-		this.desiredSpeed = desiredSpeed;
-	}
-
-	public void setDesiredTurnSpeed(double desiredTurnSpeed) {
-		this.desiredTurnSpeed = desiredTurnSpeed;
+		move(currentSpeed, currentAngle, currentTurnSpeed, absolute); // This system allows for different updating times and rates
 	}
 	
+	public static double adjust(double prevValue, double currentValue, double maxChange) {
+		if (currentValue < prevValue) {
+			if (prevValue - currentValue < maxChange) {
+				return currentValue;
+			} else {
+				return prevValue - maxChange;
+			}
+		} else {
+			if (currentValue - prevValue < maxChange) {
+				return currentValue;
+			} else {
+				return prevValue + maxChange;
+			}
+		}
+	}
+	
+	public void setDesiredSpeedDirection(double desiredSpeed, double desiredAngle) {
+		this.currentAngle = desiredAngle;
+		this.currentSpeed = desiredSpeed;
+	}
+	
+	public void setDesiredTurnSpeed(double desiredTurnSpeed) {
+		this.currentTurnSpeed = desiredTurnSpeed;
+	}
+
 	public void setAbsolute(boolean absolute) {
 		this.absolute = absolute;
 	}
