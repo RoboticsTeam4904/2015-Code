91e5633c56242d63a4440297813acfbf00fdf2dc
Team4904Classmate <botprovoking@nuevaschool.org>
2015-02-05 16:30:34 -0800

    Fixed comment typo

--- 2015-Code/src/org/usfirst/frc/team4904/robot/driver/DriverNathan.java
+++ 2015-Code/src/org/usfirst/frc/team4904/robot/driver/DriverNathan.java
@@ -8,15 +8,15 @@ import org.usfirst.frc.team4904.robot.output.Mecanum;
 
 public class DriverNathan extends Driver {
 	private final XboxController xboxController;
-	
+
 	public DriverNathan(Mecanum mecanumDrive, XboxController xboxController, AutoAlign align) {
 		super(mecanumDrive, align);
 		this.xboxController = xboxController;
 	}
-	
+
 	public synchronized void update() {
-		setMovement(-xboxController.getValue(XboxController.Y_STICK), -xboxController.getValue(XboxController.X_STICK));
-		double turnSpeed = xboxController.getValue(XboxController.TWIST_STICK) / 2; // Turns way too fast oYherwise
+		setMovement(-xboxController.getValue(XboxController.Y_STICK) * 2 / Math.PI, -xboxController.getValue(XboxController.X_STICK) * 2 / Math.PI);
+		double turnSpeed = xboxController.getValue(XboxController.TWIST_STICK) / 2; // Turns way too fast otherwise
 		setTurn(turnSpeed); // Actually do the turning
 	}
 }
