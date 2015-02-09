a0e009abaacb6b442b5d7eba7d22437c37d54f6f
leijurv <leijurv@gmail.com>
2015-02-09 11:38:14 -0800

    Removed unused function

--- 2015-Code/src/org/usfirst/frc/team4904/robot/operator/AutoOperator.java
+++ 2015-Code/src/org/usfirst/frc/team4904/robot/operator/AutoOperator.java
@@ -9,32 +9,28 @@ public class AutoOperator extends Operator {
 	public AutoOperator(Winch winch, AutoAlign align) {
 		super(winch, align);
 	}
-	
+
 	public void update() {
 		super.updateWinch();
 	}
-	
+
 	public void raise(int levels) {
 		super.raise(levels);
 	}
-	
+
 	public void lower(int levels) {
 		super.lower(levels);
 	}
-	
+
 	public void grab(int mode) {
 		super.grab(mode);
 	}
-	
+
 	public void adjust(int value) {
 		super.adjust(value);
 	}
-	
+
 	public boolean isGrabberEmpty() {
 		return super.isGrabberEmpty();
-	}
-	
-	public void disable() {
-		adjust(0);
 	}
 }
