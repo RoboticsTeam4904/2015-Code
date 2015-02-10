a85dadd90a24aba1b846ceb9a87c01aa97414036
Carter Turnbaugh <carturn@nuevaschool.org>
2015-02-09 18:22:56 -0800

    Started Auto code, merging with Yoni.

--- 2015-Code/src/org/usfirst/frc/team4904/robot/input/Camera.java
+++ 2015-Code/src/org/usfirst/frc/team4904/robot/input/Camera.java
@@ -1,3 +1,8 @@
 package org.usfirst.frc.team4904.robot.input;
 
-public class Camera {}
+
+public class Camera {
+	public double[] getYellowTote() { // This function will return the x, y coordinates of the center of the yellow tote.
+		return new double[] {0, 0}; // It uses magic.
+	}
+}
