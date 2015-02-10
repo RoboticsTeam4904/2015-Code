0666249017b5a4688c6f8fa48765374d6283c880
Carter Turnbaugh <carturn@nuevaschool.org>
2015-02-10 15:49:11 -0800

    More Kittens!

--- 2015-Code/src/org/usfirst/frc/team4904/robot/Autonomous.java
+++ 2015-Code/src/org/usfirst/frc/team4904/robot/Autonomous.java
@@ -14,33 +14,35 @@ public abstract class Autonomous implements Updatable, Disablable {
 	protected volatile int desiredWinchHeight = 0;
 	protected final Step[] steps;
 	int currentStep = 0;
+	LogKitten logger;
 	
 	public Autonomous(Step[] steps) {
 		this.steps = steps;
+		logger = new LogKitten("Autonomous", LogKitten.LEVEL_DEBUG);
 	}
 	
 	public AutoDriver getAutoDriver() {
 		return driver;
 	}
-
+	
 	public AutoOperator getAutoOperator() {
 		return operator;
 	}
-
+	
 	public void setAutoOperator(AutoOperator operator) {
 		if (this.operator != null) {
 			throw new Error("Operator already set");
 		}
 		this.operator = operator;
 	}
-
+	
 	public void setAutoDriver(AutoDriver driver) {
 		if (this.driver != null) {
 			throw new Error("Driver already set");
 		}
 		this.driver = driver;
 	}
-
+	
 	public void update() {
 		Step step = steps[currentStep];
 		boolean increase = step.run();
@@ -53,15 +55,15 @@ public abstract class Autonomous implements Updatable, Disablable {
 			currentStep++;
 		}
 	}
-
+	
 	public double[] getDesiredMovement() {
 		return new double[] {desiredXMovement, desiredYMovement, desiredTurnSpeed};
 	}
-
+	
 	public int getDesiredWinchHeight() {
 		return desiredWinchHeight;
 	}
-
+	
 	public void disable() {
 		desiredXMovement = 0;
 		desiredYMovement = 0;
