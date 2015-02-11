caf3a98ed5210df568634bd09c18501fdd3a3156
c0d3rman <yonilern@gmail.com>
2015-02-10 18:36:37 -0800

    Fixed Winch

--- 2015-Code/src/org/usfirst/frc/team4904/robot/output/Winch.java
+++ 2015-Code/src/org/usfirst/frc/team4904/robot/output/Winch.java
@@ -7,49 +7,27 @@ import edu.wpi.first.wpilibj.Talon;
 public class Winch extends Talon implements Disablable {
 	private static final int MAX_HEIGHT = 12; // each level is half a tote height
 	private int currentHeight;
-	
+
 	public Winch(int channel) {
 		super(channel);
 		currentHeight = 0;
 	}
-	
-<<<<<<< HEAD
+
 	public void setHeight(int height) { // Set winch to specific height
 		if (height > MAX_HEIGHT) {
-			height = MAX_HEIGHT;
+			currentHeight = MAX_HEIGHT;
+			return;
 		} else if (height < 0) {
-			height = 0;
-=======
-	public void setHeight(int height, boolean absolute) { // Set winch to specific height
-		if (!absolute) {
-			if (currentHeight + height > MAX_HEIGHT) {
-				currentHeight = MAX_HEIGHT;
-				return;
-			} else if (currentHeight + height < 0) {
-				currentHeight = 0;
-				return;
-			}
-			currentHeight += height;
-		} else {
-			if (height > MAX_HEIGHT) {
-				currentHeight = MAX_HEIGHT;
-				return;
-			} else if (height < 0) {
-				currentHeight = 0;
-				return;
-			}
-			currentHeight = height;
->>>>>>> 100378801a3470a0283a9954a120a617ca840a74
+			currentHeight = 0;
+			return;
 		}
+		currentHeight = height;
 	}
 	
-<<<<<<< HEAD
 	public void changeHeight(int heightChange) {
 		setHeight(currentHeight + heightChange);
 	}
 	
-=======
->>>>>>> 100378801a3470a0283a9954a120a617ca840a74
 	public void disable() {
 		set(0);
 	}
