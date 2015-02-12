f043bda7894fb7a80cf0364ac09072a926b94399
Erik Meike <erik@yoerik.com>
2015-02-11 17:56:16 -0800

    Arduino correctly reading from all encoders

--- Arduino/EncoderReader/EncoderReader.ino
+++ Arduino/EncoderReader/EncoderReader.ino
@@ -12,32 +12,44 @@
 
 #include <Wire.h>
 
-void setup()
-{
+void setup() {
   Wire.begin();        // join i2c bus (address optional for master)
   Serial.begin(115200);  // start serial for output
 }
 
-void loop()
-{
-  Wire.beginTransmission(10);
+void loop() {
+  Serial.print(readEncoder(10));
+  Serial.print("\t");
+  Serial.print(readEncoder(11));
+  Serial.print("\t");
+  Serial.print(readEncoder(12));
+  Serial.print("\t");
+  Serial.print(readEncoder(13));
+  Serial.println();
+  
+
+  delay(1);
+}
+
+long readEncoder(int i2cAddr) {
+  
+  Wire.beginTransmission(i2cAddr);
   Wire.write(0);
   delay(1); // somehow 
   Wire.endTransmission();
-  Wire.requestFrom(10, 4);    // request 6 bytes from slave device #2
+  Wire.requestFrom(i2cAddr, 4);    // request 6 bytes from slave device #2
 
   long dist = 0;
   byte counter = 0;
-  while(Wire.available())    // slave may send less than requested
-  { 
+  while(Wire.available()) {   // slave may send less than requested
+   
     byte c = Wire.read(); // receive a byte as character
-    Serial.print(c);         // print the character
-    Serial.print("\t");
+    //Serial.print(c);         // print the character
+    //Serial.print("\t");
     dist |=  c << (counter*8);
     counter++;
   }
-  Serial.print(dist);
-  Serial.println();
-
-  delay(100);
+  //Serial.print(dist);
+  //Serial.println();
+  return dist;
 }
