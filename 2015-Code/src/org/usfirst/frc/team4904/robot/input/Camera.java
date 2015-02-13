ff38ac1d846077c39ea24e226ea0f3efc4dcf1f0
Carter Turnbaugh <carturn@nuevaschool.org>
2015-02-12 16:38:25 -0800

    Added camera code.

--- 2015-Code/src/org/usfirst/frc/team4904/robot/input/Camera.java
+++ 2015-Code/src/org/usfirst/frc/team4904/robot/input/Camera.java
@@ -1,7 +1,17 @@
 package org.usfirst.frc.team4904.robot.input;
 
 
+import edu.wpi.first.wpilibj.CameraServer;
+
 public class Camera {
+	private CameraServer server;
+	
+	public Camera() {
+		server = CameraServer.getInstance();
+		server.setQuality(50);
+		server.startAutomaticCapture("cam0");
+	}
+	
 	public double[] getYellowTote() { // This function will return the x, y coordinates of the center of the yellow tote.
 		return new double[] {0, 0}; // It uses magic.
 	}
