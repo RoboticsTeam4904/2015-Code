5167f10120ea014bfcfee4a45b41e0501520ba7e
Carter Turnbaugh <carturn@nuevaschool.org>
2015-02-14 13:11:14 -0800

    Stopped disabling UDAR (passive, doesn't need disable).

--- 2015-Code/src/org/usfirst/frc/team4904/robot/input/UDAR.java
+++ 2015-Code/src/org/usfirst/frc/team4904/robot/input/UDAR.java
@@ -2,12 +2,11 @@ package org.usfirst.frc.team4904.robot.input;
 
 
 import java.math.BigInteger;
-import org.usfirst.frc.team4904.robot.Disablable;
 import org.usfirst.frc.team4904.robot.SuperPort;
 import org.usfirst.frc.team4904.robot.Updatable;
 import edu.wpi.first.wpilibj.I2C;
 
-public class UDAR extends SuperPort implements Updatable, Disablable {
+public class UDAR extends SuperPort implements Updatable {
 	private final I2C i2c;
 	private final int numSensors = 3;
 	
@@ -28,9 +27,5 @@ public class UDAR extends SuperPort implements Updatable, Disablable {
 				data[i] = new BigInteger(new byte[] {0, I2CData[2 * i], I2CData[2 * i + 1]}).intValue();
 			}
 		}
-	}
-	
-	public void disable() {
-		// TODO Auto-generated method stub
 	}
 }
