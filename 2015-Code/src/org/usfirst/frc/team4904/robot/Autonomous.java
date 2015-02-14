ed96a3dfc3ac0a948fb14e57bc3b3d9180efb963
leijurv <leijurv@gmail.com>
2015-02-14 09:19:00 -0800

    Reset speed after step change

--- 2015-Code/src/org/usfirst/frc/team4904/robot/Autonomous.java
+++ 2015-Code/src/org/usfirst/frc/team4904/robot/Autonomous.java
@@ -16,34 +16,34 @@ public abstract class Autonomous implements Updatable, Disablable {
 	protected final Step[] steps;
 	int currentStep = 0;
 	LogKitten logger;
-	
+
 	public Autonomous(Step[] steps) {
 		this.steps = steps;
 		logger = new LogKitten("Autonomous", LogKitten.LEVEL_DEBUG);
 	}
-	
+
 	public AutoDriver getAutoDriver() {
 		return driver;
 	}
-	
+
 	public AutoOperator getAutoOperator() {
 		return operator;
 	}
-	
+
 	public void setAutoOperator(AutoOperator operator) {
 		if (this.operator != null) {
 			throw new Error("Operator already set");
 		}
 		this.operator = operator;
 	}
-	
+
 	public void setAutoDriver(AutoDriver driver) {
 		if (this.driver != null) {
 			throw new Error("Driver already set");
 		}
 		this.driver = driver;
 	}
-	
+
 	public void update() {
 		Step step = steps[currentStep];
 		step.setCurrentWinchHeight(currentWinchHeight);
@@ -55,21 +55,25 @@ public abstract class Autonomous implements Updatable, Disablable {
 		this.desiredYMovement = movement[1];
 		if (increase) {
 			currentStep++;
+			this.desiredTurnSpeed = 0;
+			this.desiredWinchHeight = this.currentWinchHeight;
+			this.desiredXMovement = 0;
+			this.desiredYMovement = 0;
 		}
 	}
-	
+
 	public double[] getDesiredMovement() {
 		return new double[] {desiredXMovement, desiredYMovement, desiredTurnSpeed};
 	}
-	
+
 	public int getDesiredWinchHeight() {
 		return desiredWinchHeight;
 	}
-	
+
 	public void setCurrentWinchHeight(int currentWinchHeight) {
 		this.currentWinchHeight = currentWinchHeight;
 	}
-	
+
 	public void disable() {
 		desiredXMovement = 0;
 		desiredYMovement = 0;
