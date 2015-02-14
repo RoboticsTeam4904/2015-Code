59c8563996358198bfe6a1f428c182deeb1d7ab5
leijurv <leijurv@gmail.com>
2015-02-14 09:11:51 -0800

    Fixed very fast releasing

--- 2015-Code/src/org/usfirst/frc/team4904/robot/autonomous/autosteps/StackTote.java
+++ 2015-Code/src/org/usfirst/frc/team4904/robot/autonomous/autosteps/StackTote.java
@@ -5,18 +5,20 @@ import org.usfirst.frc.team4904.robot.AutoAlign;
 import org.usfirst.frc.team4904.robot.autonomous.Step;
 
 public class StackTote extends Step {
-	private AutoAlign align;
-	
+	private final AutoAlign align;
+
 	public StackTote(AutoAlign align) {
 		this.align = align;
 	}
-	
+
 	public boolean run() {
 		desiredXMovement = 1;
 		if (align.isGrabberEmpty()) {
 			return true;
 		} else {
-			align.release();
+			if (!align.isCurrentlyReleasing()) {
+				align.release();
+			}
 			return false;
 		}
 	}
