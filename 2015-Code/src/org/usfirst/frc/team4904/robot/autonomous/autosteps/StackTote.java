4bc593ba68e27dcdb1ecfdaeadde1e64143d9294
leijurv <leijurv@gmail.com>
2015-02-14 09:13:56 -0800

    Fixed movement

--- 2015-Code/src/org/usfirst/frc/team4904/robot/autonomous/autosteps/StackTote.java
+++ 2015-Code/src/org/usfirst/frc/team4904/robot/autonomous/autosteps/StackTote.java
@@ -6,13 +6,12 @@ import org.usfirst.frc.team4904.robot.autonomous.Step;
 
 public class StackTote extends Step {
 	private final AutoAlign align;
-
+	
 	public StackTote(AutoAlign align) {
 		this.align = align;
 	}
-
+	
 	public boolean run() {
-		desiredXMovement = 1;
 		if (align.isGrabberEmpty()) {
 			return true;
 		} else {
