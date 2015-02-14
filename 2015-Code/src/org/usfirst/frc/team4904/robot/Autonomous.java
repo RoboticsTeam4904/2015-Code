45b5aefb1ea63cee5173e15ee4c0a1bd7efcaa6d
leijurv <leijurv@gmail.com>
2015-02-14 09:24:19 -0800

    Made autonomous not abstract

--- 2015-Code/src/org/usfirst/frc/team4904/robot/Autonomous.java
+++ 2015-Code/src/org/usfirst/frc/team4904/robot/Autonomous.java
@@ -5,7 +5,7 @@ import org.usfirst.frc.team4904.robot.autonomous.Step;
 import org.usfirst.frc.team4904.robot.driver.AutoDriver;
 import org.usfirst.frc.team4904.robot.operator.AutoOperator;
 
-public abstract class Autonomous implements Updatable, Disablable {
+public class Autonomous implements Updatable, Disablable {
 	private AutoOperator operator = null;
 	private AutoDriver driver = null;
 	protected volatile double desiredXMovement = 0;
@@ -16,34 +16,34 @@ public abstract class Autonomous implements Updatable, Disablable {
 	protected final Step[] steps;
 	int currentStep = 0;
 	LogKitten logger;
-	
-	public Autonomous(Step[] steps) {
+
+	protected Autonomous(Step[] steps) {
 		this.steps = steps;
 		logger = new LogKitten("Autonomous", LogKitten.LEVEL_DEBUG);
 	}
-	
+
 	public AutoDriver getAutoDriver() {
 		return driver;
 	}
-	
+
 	public AutoOperator getAutoOperator() {
 		return operator;
 	}
-	
+
 	public void setAutoOperator(AutoOperator operator) {
 		if (this.operator != null) {
 			throw new Error("Operator already set");
 		}
 		this.operator = operator;
 	}
-	
+
 	public void setAutoDriver(AutoDriver driver) {
 		if (this.driver != null) {
 			throw new Error("Driver already set");
 		}
 		this.driver = driver;
 	}
-	
+
 	public void update() {
 		Step step = steps[currentStep];
 		step.setCurrentWinchHeight(currentWinchHeight);
@@ -58,19 +58,19 @@ public abstract class Autonomous implements Updatable, Disablable {
 			disable(); // A step might leave the desiredMovement variables in a nonzero state
 		}
 	}
-	
+
 	public double[] getDesiredMovement() {
 		return new double[] {desiredXMovement, desiredYMovement, desiredTurnSpeed};
 	}
-	
+
 	public int getDesiredWinchHeight() {
 		return desiredWinchHeight;
 	}
-	
+
 	public void setCurrentWinchHeight(int currentWinchHeight) {
 		this.currentWinchHeight = currentWinchHeight;
 	}
-	
+
 	public void disable() {
 		desiredXMovement = 0;
 		desiredYMovement = 0;
