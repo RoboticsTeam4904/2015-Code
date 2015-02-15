2e071a64d8bbe816e03adab52420a1dbd7cba6ab
Erik Meike <erik@yoerik.com>
2015-02-14 16:13:18 -0800

    Very basic PID trainer

--- 2015-Code/src/org/usfirst/frc/team4904/robot/output/PID.java
+++ 2015-Code/src/org/usfirst/frc/team4904/robot/output/PID.java
@@ -1,6 +1,7 @@
 package org.usfirst.frc.team4904.robot.output;
 
 
+import org.usfirst.frc.team4904.robot.LogKitten;
 import org.usfirst.frc.team4904.robot.Robot;
 
 public class PID {
@@ -9,25 +10,53 @@ public class PID {
 	private double D;
 	private double integralSum;
 	private double previousError;
+	private double absoluteError;
+	private double lastAbsoluteError;
+	private LogKitten logger;
+	private double[] lastPID;
 	
 	public PID(double P, double I, double D) {
 		this.P = P;
 		this.I = I;
 		this.D = D;
+		this.absoluteError = 0;
+		logger = new LogKitten("PID", LogKitten.LEVEL_DEBUG, LogKitten.LEVEL_FATAL);
 		reset();
 	}
 	
 	public void reset() {
 		previousError = 0;
 		integralSum = 0;
+		absoluteError = 0;
 	}
 	
 	public double calculate(double target, double current) {
-		double error = target - current;
-		double pComponent = error * P / Robot.fastUpdatePeriod;
-		double dComponent = (error - previousError) * D / Robot.fastUpdatePeriod;
+		double error = (target - current) / Robot.fastUpdatePeriod;
+		double pComponent = error * P;
+		double dComponent = (error - previousError) * D;
 		previousError = error;
-		double iComponent = (integralSum += error) * I / Robot.fastUpdatePeriod;
+		integralSum += error;
+		double iComponent = integralSum * I;
+		// Update total error
+		this.absoluteError += Math.abs(error);
 		return pComponent + iComponent + dComponent;
+	}
+	
+	public void train() {
+		logger.v("EndTrainP", Double.toString(P));
+		logger.v("EndtrainI", Double.toString(I));
+		logger.v("EndtrainD", Double.toString(D));
+		logger.v("EndtrainError", Double.toString(absoluteError));
+		reset();
+		double[] newPID = new double[3];
+		double errorDiff = lastAbsoluteError - absoluteError;
+		newPID[0] = errorDiff * P * 0.1;
+		newPID[1] = errorDiff * I * 0.1;
+		newPID[2] = errorDiff * D * 0.1;
+		lastPID = new double[] {P, I, D};
+		lastAbsoluteError = absoluteError;
+		P = newPID[0];
+		I = newPID[1];
+		D = newPID[2];
 	}
 }
