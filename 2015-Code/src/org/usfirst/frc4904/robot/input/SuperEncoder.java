c9dd69b018f7f0bf7158f12bbeefe18aa8bd6a14
Carter Turnbaugh <carturn@nuevaschool.org>
2015-02-15 19:03:56 -0800

    Added SuperEncoders to SuperSerial stuff.

--- 2015-Code/src/org/usfirst/frc4904/robot/input/SuperEncoder.java
+++ 2015-Code/src/org/usfirst/frc4904/robot/input/SuperEncoder.java
@@ -3,23 +3,19 @@ package org.usfirst.frc4904.robot.input;
 
 import java.math.BigInteger;
 import org.usfirst.frc4904.robot.LogKitten;
-import edu.wpi.first.wpilibj.I2C;
 
 public class SuperEncoder {
-	private I2C i2c;
+	private SuperSerial port;
 	private int previousNumTicks;
 	private double previousTime;
 	private LogKitten logger;
+	private int channel;
 	
-	public SuperEncoder(int i2cAddr) {
-		try {
-			i2c = new I2C(I2C.Port.kOnboard, i2cAddr);
-		}
-		catch (NullPointerException e) {
-			i2c = null;
-		}
+	public SuperEncoder(int channel, SuperSerial port) {
+		this.port = port;
 		previousNumTicks = getTicks();
-		logger = new LogKitten("SuperEncoder" + Integer.toString(i2cAddr), LogKitten.LEVEL_DEBUG, LogKitten.LEVEL_VERBOSE);
+		this.channel = channel;
+		logger = new LogKitten("SuperEncoder" + Integer.toString(channel), LogKitten.LEVEL_DEBUG, LogKitten.LEVEL_VERBOSE);
 	}
 	
 	public double currentEncoderSpeed() { // Should return ticks per second
@@ -37,20 +33,9 @@ public class SuperEncoder {
 	}
 	
 	public int getTicks() {
-		if (i2c == null) {
-			return 0;
-		}
-		int value;
-		try {
-			byte[] toRecieve = new byte[4];
-			byte[] toSend = new byte[1];
-			toSend = new byte[] {0};
-			i2c.transaction(toSend, 1, toRecieve, 4);
-			value = new BigInteger(toRecieve).intValue();
-		}
-		catch (NullPointerException e) {
-			value = 0;
-		}
+		byte[] toRecieve = new byte[4];
+		toRecieve = port.readEncoder(this.channel, 4);
+		int value = new BigInteger(toRecieve).intValue();
 		return value;
 	}
 	
