8714997dfd2fca1af03a16a32f83aec4344510bd
Carter Turnbaugh <carturn@nuevaschool.org>
2015-02-16 14:18:08 -0800

    Setup MPU9150 to work with SuperSerial. Also added more Kittens.

--- 2015-Code/src/org/usfirst/frc4904/robot/input/MPU9150.java
+++ 2015-Code/src/org/usfirst/frc4904/robot/input/MPU9150.java
@@ -1,16 +1,19 @@
 package org.usfirst.frc4904.robot.input;
 
-import org.usfirst.frc4904.robot.Updatable;
 
+import org.usfirst.frc4904.robot.LogKitten;
+import org.usfirst.frc4904.robot.Updatable;
 
 public class MPU9150 implements Updatable {
 	private double movementMagnitude;
 	private double movementAngle;
 	private double absoluteAngle;
 	private final SuperSerial serial;
+	LogKitten logger;
 	
 	public MPU9150(SuperSerial serial) {
 		this.serial = serial;
+		logger = new LogKitten("MPU9150", LogKitten.LEVEL_VERBOSE, LogKitten.LEVEL_VERBOSE);
 	}
 	
 	public void init() {}
@@ -24,6 +27,7 @@ public class MPU9150 implements Updatable {
 	}
 	
 	public void update() {
-		byte[] data = serial.readIMU(0);
+		String data = serial.readIMU(0);
+		logger.v("update", data);
 	}
 }
