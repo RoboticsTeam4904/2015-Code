df48ce941f7bb921c971b7dc2d2a72666c4a5c5e
Carter Turnbaugh <carter@localhost.localdomain>
2015-02-16 23:06:36 -0800

    ArrayLists in SuperSerial.

--- 2015-Code/src/org/usfirst/frc4904/robot/input/SuperSerial.java
+++ 2015-Code/src/org/usfirst/frc4904/robot/input/SuperSerial.java
@@ -2,26 +2,28 @@ package org.usfirst.frc4904.robot.input;
 
 
 // We are combining multiple serial streams, this processes those.
+import java.util.ArrayList;
 import org.usfirst.frc4904.robot.LogKitten;
 import org.usfirst.frc4904.robot.Updatable;
 import edu.wpi.first.wpilibj.SerialPort;
 
 public class SuperSerial implements Updatable {
 	private final SerialPort port;
-	private volatile String lidarData;
-	private volatile String udarData;
-	private volatile String imuData;
-	private volatile String[] encoderData = new String[5];
+	private volatile ArrayList<String> lidarData;
+	private volatile ArrayList<String> udarData;
+	private volatile ArrayList<String> imuData;
+	private volatile ArrayList<String>[] encoderData;
 	private static final int NUM_LEDS = 209;
 	private final LogKitten logger;
 	
 	public SuperSerial() {
 		port = new SerialPort(115200, SerialPort.Port.kMXP);
-		lidarData = "";
-		udarData = "";
-		imuData = "";
+		lidarData = new ArrayList<String>();
+		udarData = new ArrayList<String>();
+		imuData = new ArrayList<String>();
+		encoderData = new ArrayList[5];
 		for (int i = 0; i < 5; i++) {
-			encoderData[i] = "";
+			encoderData[i] = new ArrayList<String>();
 		}
 		logger = new LogKitten("SuperSerial", LogKitten.LEVEL_DEBUG, LogKitten.LEVEL_VERBOSE);
 	}
@@ -37,7 +39,7 @@ public class SuperSerial implements Updatable {
 				data += current;
 			}
 			logger.v("update", "Got data " + data);
-			imuData = data;
+			imuData.add(data);
 			/*
 			 * logger.v("update", port.getBytesReceived() + " bytes received"); logger.v("update", "Got data " + data); String[] lines = data.split("\n"); for (String line : lines) { logger.v("line", line); line = line.substring(0, line.length() - 1); if (line.startsWith("LIDAR")) { line = line.substring(5); lidarData += "#" + line + "$"; } else if (line.startsWith("UDAR")) { line = line.substring(4); udarData += "#" + line + "$"; } else if (line.startsWith("IMU")) { line = line.substring(3); logger.v("imuData", imuData); imuData = line; logger.v("Update", "Added " + line + " to IMU"); } else if (line.startsWith("E0")) { line = line.substring(2); encoderData[0] += "#" + line + "$"; } else if (line.startsWith("E1")) { line = line.substring(2); encoderData[1] += "#" + line + "$"; } else if (line.startsWith("E2")) { line = line.substring(2); encoderData[2] += "#" + line + "$"; } else if (line.startsWith("E3")) { line = line.substring(2); encoderData[3] += "#" + line + "$"; } else if (line.startsWith("E4")) { line = line.substring(2); encoderData[4] += "#" + line + "$"; } }
 			 */
@@ -45,75 +47,43 @@ public class SuperSerial implements Updatable {
 	}
 	
 	public String readLIDAR() { // Reads the first available full LIDAR pulse
-		String data = "";
-		int iter = 0;
-		while (lidarData.charAt(iter) != '#') {
-			iter++;
-		}
-		while (lidarData.charAt(iter) != '$') {
-			data += lidarData.charAt(iter);
-			iter++;
-		}
-		lidarData = lidarData.substring(iter, lidarData.length());
+		String data = lidarData.get(0);
+		lidarData.remove(0);
 		return data;
 	}
 	
 	public int availableLIDARData() {
-		return lidarData.length();
+		return lidarData.size();
 	}
 	
 	public String readUDAR() { // Reads the first available full UDAR pulse
-		String data = "";
-		int iter = 0;
-		while (udarData.charAt(iter) != '#') {
-			iter++;
-		}
-		while (udarData.charAt(iter) != '$') {
-			data += udarData.charAt(iter);
-			iter++;
-		}
-		udarData = udarData.substring(iter, udarData.length());
+		String data = udarData.get(0);
+		udarData.remove(0);
 		return data;
 	}
 	
 	public int availableUDARData() {
-		return udarData.length();
+		return udarData.size();
 	}
 	
 	public String readEncoder(int encoder) { // Reads the first available full encoder pulse
-		String data = "";
-		int iter = 0;
-		while (encoderData[encoder].charAt(iter) != '#') {
-			iter++;
-		}
-		while (encoderData[encoder].charAt(iter) != '$') {
-			data += encoderData[encoder].charAt(iter);
-			iter++;
-		}
-		encoderData[encoder] = encoderData[encoder].substring(iter, imuData.length());
+		String data = encoderData[encoder].get(0);
+		encoderData[encoder].remove(0);
 		return data;
 	}
 	
 	public int availableEncoderData(int channel) {
-		return encoderData[channel].length();
+		return encoderData[channel].size();
 	}
 	
 	public String readIMU() { // Reads the first available full IMU pulse
-		String data = "";
-		int iter = 0;
-		while (imuData.charAt(iter) != '#') {
-			iter++;
-		}
-		while (imuData.charAt(iter) != '$') {
-			data += imuData.charAt(iter);
-			iter++;
-		}
-		imuData = imuData.substring(iter, imuData.length());
+		String data = imuData.get(0);
+		imuData.remove(0);
 		return data;
 	}
 	
 	public int availableIMUData() {
-		return imuData.length();
+		return imuData.size();
 	}
 	
 	public void setLeds(byte[] leds) {
