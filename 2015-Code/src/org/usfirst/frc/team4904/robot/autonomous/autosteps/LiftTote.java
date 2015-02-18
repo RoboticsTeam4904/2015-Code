3473669490fddceeaecee62e5aa0668d7f4dd25c
leijurv <leijurv@gmail.com>
2015-02-17 20:22:41 -0700

    Fixed LiftTote

--- 2015-Code/src/org/usfirst/frc/team4904/robot/autonomous/autosteps/LiftTote.java
+++ 2015-Code/src/org/usfirst/frc/team4904/robot/autonomous/autosteps/LiftTote.java
@@ -6,6 +6,6 @@ import org.usfirst.frc.team4904.robot.autonomous.Step;
 public class LiftTote extends Step {
 	public boolean run() {
 		desiredWinchHeight = 12;
-		return false;
+		return currentWinchHeight == 12;
 	}
 }
