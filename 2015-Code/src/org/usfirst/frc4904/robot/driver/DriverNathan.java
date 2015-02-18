e54413e5c69171898f72b93fe3730f0763560539
Carter Turnbaugh <carturn@nuevaschool.org>
2015-02-17 18:11:10 -0800

    Adjusted control stick scaling.

--- 2015-Code/src/org/usfirst/frc4904/robot/driver/DriverNathan.java
+++ 2015-Code/src/org/usfirst/frc4904/robot/driver/DriverNathan.java
@@ -9,6 +9,7 @@ import org.usfirst.frc4904.robot.output.Mecanum;
 public class DriverNathan extends Driver {
 	private final XboxController xboxController;
 	private final LogKitten logger;
+	private static final double MathPHI = 1.618;
 	
 	public DriverNathan(Mecanum mecanumDrive, XboxController xboxController, AutoAlign align) {
 		super(mecanumDrive, align);
@@ -17,10 +18,11 @@ public class DriverNathan extends Driver {
 	}
 	
 	public synchronized void update() {
-		setMovement(xboxController.getValue(XboxController.X_STICK) * 2 / Math.PI, -xboxController.getValue(XboxController.Y_STICK) * 2 / Math.PI);
+		setMovement(xboxController.getValue(XboxController.X_STICK) * MathPHI / Math.PI, -xboxController.getValue(XboxController.Y_STICK) * MathPHI / Math.PI);
 		// We have to compute the inverse of the fourth derivative of cosine at -1 because the radius of the wheel is a complex number with an i component of 0.
 		// This forces us to compensate with multiplying the movement by this factor in order to prevent the sqrt function from overloading during the update of the mecanum.
 		// If we don't do this, the entire code may fail. ||_|_|_||\/|||\|∆T|
+		// PHI is used because the golden ration is pretty.
 		double turnSpeed = xboxController.getValue(XboxController.TWIST_STICK) / 2; // Turns way too fast otherwise
 		setTurn(turnSpeed); // Actually do the turning
 		if (xboxController.getButton(xboxController.A_BUTTON)) {
