9d0f80007a8ec82d6f7cc863349831d494c2fa74
Carter Turnbaugh <carterturn@yahoo.com>
2015-03-16 22:02:30 -0700

    Lowered winch before releasing tote.

--- 2015-Code/src/org/usfirst/frc4904/robot/autonomous/OneToteMove.java
+++ 2015-Code/src/org/usfirst/frc4904/robot/autonomous/OneToteMove.java
@@ -4,6 +4,7 @@ package org.usfirst.frc4904.robot.autonomous;
 import org.usfirst.frc4904.robot.autonomous.autosteps.BackwardsCharge;
 import org.usfirst.frc4904.robot.autonomous.autosteps.GrabTote;
 import org.usfirst.frc4904.robot.autonomous.autosteps.LiftTote;
+import org.usfirst.frc4904.robot.autonomous.autosteps.LowerGrabber;
 import org.usfirst.frc4904.robot.autonomous.autosteps.OpenGrabber;
 import org.usfirst.frc4904.robot.autonomous.autosteps.TimedDrive;
 import org.usfirst.frc4904.robot.input.IMU;
@@ -11,6 +12,6 @@ import org.usfirst.frc4904.robot.output.Grabber;
 
 public class OneToteMove extends Autonomous {
 	protected OneToteMove(IMU imu, Grabber grabber) {
-		super(new Step[] {new GrabTote(grabber), new LiftTote(1), new BackwardsCharge(imu), new TimedDrive(0, 0.1, 0.2), new OpenGrabber(grabber)});
+		super(new Step[] {new GrabTote(grabber), new LiftTote(1), new BackwardsCharge(imu), new TimedDrive(0, 0.1, 0.2), new LowerGrabber(0), new OpenGrabber(grabber)});
 	}
 }
