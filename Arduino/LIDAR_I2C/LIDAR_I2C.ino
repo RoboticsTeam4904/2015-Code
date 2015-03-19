aad6d7d8b9e32a6b9d289d70e147c6c8dbccb749
Carter Turnbaugh <carturn@nuevaschool.org>
2015-03-19 09:13:02 -0700

    Added new LIDAR Arduino code.

---
+++ Arduino/LIDAR_I2C/LIDAR_I2C.ino
@@ -0,0 +1,169 @@
+#include <Wire.h>
+
+double angle = 0;
+unsigned int distance_array[360];
+unsigned char flag1 = 0;
+unsigned char flag2 = 0;
+int inByte = 0; // incoming serial byte
+unsigned char Data_status = 0;
+unsigned char Data_4deg_index = 0;
+unsigned char Data_loop_index = 0;
+int dataIndex;
+
+
+void setup() {
+  delay(500);
+  Serial.begin(115200); // USB serial
+  while (!Serial) {}
+  Serial.println("Arduino Neato XV-11 Motor control board v0.1 by Cheng-Lung Lee, modified by Andrew");
+
+  Serial1.begin(115200); // LIDAR serial
+  while (!Serial1) {}
+  Serial.println("LIDAR connected");
+
+  Wire.begin(2);
+  Wire.onRequest(sendData);
+}
+
+void loop() {
+  // if we get a valid byte from LDS, read it and send it to USB-serial
+  if(Serial1.available() > 0) {
+    // get incoming byte:
+    inByte = Serial1.read();
+    //Serial.write(inByte);
+    decodeData(inByte);
+    if (dataIndex == 2 && Data_4deg_index == 89) {
+      Data_status = 0; Data_4deg_index = 0; Data_loop_index = 0;
+
+      Serial.print(distance_array[100]);
+    }
+  }
+
+  if(Serial.available() > 0) {
+    String inString = "";
+    bool finished = false;
+    while(Serial.available() > 0 && !finished) {
+      int inChar = Serial.read();
+      if(isDigit(inChar)) inString += (char) inChar;
+      if(inChar == '\n') finished = true;
+    }
+    int index = inString.toInt();
+    Serial.print("index: ");
+    Serial.print(index);
+    Serial.print("distance: ");
+    Serial.print(distance_array[index]);
+    Serial.println();
+  }
+}
+
+void sendData() {
+  String inString = "";
+  bool finished = false;
+  while(Wire.available() && !finished) {
+    int inChar = Wire.read();
+    if(isDigit(inChar)) inString += (char) inChar;
+    if(inChar == '\n') finished = true;
+  }
+  int index = inString.toInt();
+  String data = String(distance_array[index], DEC);
+  Wire.write(data.c_str());
+}
+
+void decodeData(unsigned char inByte) {
+  switch(Data_status) {
+    case 0: // no header
+      if (inByte == 0xFA) {
+        Data_status = 1;
+        Data_loop_index = 1;
+      }
+      break;
+    case 1: // Find 2nd FA
+      if (Data_loop_index == 22) {
+        if (inByte == 0xFA) {
+          Data_status = 2;
+          Data_loop_index = 1;
+        }
+        else // if not FA search again
+          Data_status = 0;
+      }
+      else {
+        Data_loop_index++;
+      }
+      break;
+    case 2: // Read data out
+      if (Data_loop_index == 22) {
+        if (inByte == 0xFA) {
+          Data_loop_index = 1;
+        }
+        else // if not FA search again
+          Data_status = 0;
+      }
+      else {
+        readData(inByte);
+        Data_loop_index++;
+      }
+      break;
+  }
+}
+void readData(unsigned char inByte) {
+
+  unsigned char distanceSnips [4] [2];
+  switch (Data_loop_index) {
+    case 1: // 4 degree index
+      Data_4deg_index = inByte - 0xA0;
+      break;
+    case 2: // Speed in RPH low byte
+      // #dontcare
+      break;
+    case 3: // Speed in RPH high byte
+      // #dontcare
+      break;
+    case 4:
+      distanceSnips[0][0] = inByte;
+      break;
+    case 5:
+      dataIndex = 0;
+      distanceSnips[dataIndex][1] = inByte & B00111111; //mask 14 and 15 bits
+      parseDistance(distanceSnips[dataIndex][1], distanceSnips[dataIndex][0]);
+      break;
+    case 8:
+      distanceSnips[1][0] = inByte;
+      break;
+    case 9:
+      dataIndex = 1;
+      distanceSnips[dataIndex][1] = inByte & B00111111; //mask 14 and 15 bits
+      parseDistance(distanceSnips[dataIndex][1], distanceSnips[dataIndex][0]);
+      break;
+    case 12:
+      distanceSnips[2][0] = inByte;
+      break;
+    case 13:
+      dataIndex = 2;
+      distanceSnips[dataIndex][1] = inByte & B00111111; //mask 14 and 15 bits
+      parseDistance(distanceSnips[dataIndex][1], distanceSnips[dataIndex][0]);
+      break;
+    case 16:
+      distanceSnips[3][0] = inByte;
+      break;
+    case 17:
+      dataIndex = 3;
+      distanceSnips[dataIndex][1] = inByte & B00111111; //mask 14 and 15 bits
+      parseDistance(distanceSnips[dataIndex][1], distanceSnips[dataIndex][0]);
+      break;
+    default:
+      break;
+  }
+}
+int parseDistance(unsigned char distanceHigh, unsigned char distanceLow) {
+  flag1 = inByte & B10000000;
+  flag2 = inByte & B01000000;
+  int distance = (distanceHigh << 8) | distanceLow;
+  int angleIndex = Data_4deg_index * 4 + dataIndex;
+  if (flag1 == 0 && flag2 == 0) { // invalid data and strength warning flags
+    distance_array[angleIndex] = distance;
+  }
+  else {
+    distance_array[angleIndex] = 0;
+  }
+}
+
