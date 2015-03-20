e34d6bac8a2df680b60713387d24edb30e67cd4c
Team4904Classmate <botprovoking@nuevaschool.org>
2015-03-20 09:55:07 -0700

    Winch fix

--- 2015-Code/src/org/usfirst/frc4904/robot/output/Winch.java
+++ 2015-Code/src/org/usfirst/frc4904/robot/output/Winch.java
@@ -12,7 +12,7 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class Winch extends Talon implements Disablable, Enablable, Updatable, PIDOutput {
 	private static final double MAX_HEIGHT = 12; // each level is half a tote height off the bottom-most position of the winch
-	private static final double DISTANCE_PER_PULSE = 1.0 / 28.75; // TODO Some number of ticks is one level. Needs to be determined
+	private static final double DISTANCE_PER_PULSE = (58.0 / 6.0) / 344.00; // Makerslide height: 57-58". Half-tote: 6". Winch pulses per makerslide: 344.
 	private final Encoder encoder;
 	private final PIDController pid;
 	private boolean overridePID = true;
@@ -77,10 +77,10 @@ public class Winch extends Talon implements Disablable, Enablable, Updatable, PI
 	public void overrideSet(double speed) {
 		if (Math.abs(speed) > 0.05 && pid.isEnable()) {
 			pid.disable();
-			super.set(speed);
 		} else if (Math.abs(speed) < 0.05 && !overridePID) {
 			pid.enable();
-		} else if (Math.abs(speed) < 0.05) {
+		}
+		if (!pid.isEnable()) {
 			super.set(speed);
 		}
 	}
