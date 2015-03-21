79e72b5e04d2e00d32773592f794df138d4a1192
c0d3rman <yonilern@gmail.com>
2015-03-20 22:30:07 -0700

    Refactored BackwardsMove to TimedBackwardsMove

--- 2015-Code/src/org/usfirst/frc4904/robot/autonomous/TimedBackwardsMove.java
+++ 2015-Code/src/org/usfirst/frc4904/robot/autonomous/TimedBackwardsMove.java
@@ -3,8 +3,8 @@ package org.usfirst.frc4904.robot.autonomous;
 
 import org.usfirst.frc4904.robot.autonomous.autosteps.TimedDrive;
 
-public class BackwardsMove extends Autonomous {
-	protected BackwardsMove() {
+public class TimedBackwardsMove extends Autonomous {
+	protected TimedBackwardsMove() {
 		super(new Step[] {new TimedDrive(0, -0.5, 2)});
 	}
 }
