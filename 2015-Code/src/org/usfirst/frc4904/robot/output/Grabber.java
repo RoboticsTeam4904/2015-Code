0917501415040c7f692117fa2234a2407368b05a
Carter Turnbaugh <carturn@nuevaschool.org>
2015-03-21 11:07:33 -0700

    Making Grabber stop better.

--- 2015-Code/src/org/usfirst/frc4904/robot/output/Grabber.java
+++ 2015-Code/src/org/usfirst/frc4904/robot/output/Grabber.java
@@ -13,8 +13,10 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 public class Grabber extends Talon implements Disablable, Updatable {
 	public static final int RIGHT_OUTER_SWITCH = 0;
 	public static final int LEFT_OUTER_SWITCH = 1;
+	public static final int INNER_SWITCH = 2;
 	public static final int PDP_PORT = 1;
-	private static final double MAX_AMPS = 12; // Tune this value
+	private static final double MAX_AMPS = 8; // Tune this value
+	private static final double LIMIT_AMPS = 15;
 	private static final int NUM_PAST_CURRENTS = (int) (0.25 / Robot.fastUpdatePeriod); // Number of past currents to average
 	private final DigitalInput[] limitSwitches;
 	private LogKitten logger;
@@ -27,7 +29,7 @@ public class Grabber extends Talon implements Disablable, Updatable {
 	private int currentPosition = 0;
 	
 	public enum GrabberState { // an enum containing grabber states and their values
-		OPEN(0), CLOSED(-0.1), OPENING(0.5), CLOSING(-0.5), DISABLED(0), FREEZE(-0.075), FIXING(-0.2); // grabber state and values
+		OPEN(0), CLOSED(-0.0), OPENING(0.5), CLOSING(-0.5), DISABLED(0); // grabber state and values
 		public final double motorSpeed; // the architecture allowing the enum states to have values
 		
 		private GrabberState(double speed) {
@@ -71,10 +73,6 @@ public class Grabber extends Talon implements Disablable, Updatable {
 				grabberState = GrabberState.CLOSING;
 				logger.w("setDesiredGrabberState", "Setting state to closing");
 				break;
-			case FREEZE:
-				grabberState = GrabberState.FREEZE;
-				logger.w("setDesiredGrabberState", "Setting state to freeze");
-				break;
 			default:
 				throw new Error("Invalid or unsupported state passed to setDesiredGrabberState");
 		}
@@ -105,7 +103,6 @@ public class Grabber extends Talon implements Disablable, Updatable {
 	private void checkLimitSwitches() {
 		switch (grabberState) {
 			case OPENING:
-			case FIXING:
 				if (!limitSwitches[RIGHT_OUTER_SWITCH].get()) { // If limit switch has been hit (get() returns opposite - true if not pressed)
 					logger.v("checkLimitSwitches", "Right outer switch");
 					grabberState = GrabberState.OPEN; // Don't go too far
@@ -115,6 +112,11 @@ public class Grabber extends Talon implements Disablable, Updatable {
 					grabberState = GrabberState.OPEN; // Don't go too far
 				}
 				return;
+			case CLOSING:
+				if (!limitSwitches[INNER_SWITCH].get()) {
+					logger.v("checkLimitSwitches", "Inner switch");
+					grabberState = GrabberState.DISABLED;
+				}
 			default:
 				return;
 		}
@@ -133,6 +135,9 @@ public class Grabber extends Talon implements Disablable, Updatable {
 			// if (this.get() < 0) negate = 1;
 			// else if (this.get() > 0) negate = -1;
 			logger.f("checkPowerUsage", "stopped close " + currentCurrent);
+		}
+		if (currentCurrent > LIMIT_AMPS) {
+			grabberState = GrabberState.DISABLED;
 		}
 	}
 	
