2db20a6827d0e6489b801b21ef43c33e02660107
Carter Turnbaugh <carturn@nuevaschool.org>
2015-03-24 12:11:56 -0700

    Added active detection to LogitechJoystick.

--- 2015-Code/src/org/usfirst/frc4904/robot/input/LogitechJoystick.java
+++ 2015-Code/src/org/usfirst/frc4904/robot/input/LogitechJoystick.java
@@ -5,11 +5,24 @@ import edu.wpi.first.wpilibj.Joystick;
 
 public class LogitechJoystick extends Joystick {
 	public SuperButton[] buttons = new SuperButton[12];
+	public static final int X_AXIS = 0;
+	public static final int Y_AXIS = 1;
+	private static final double moveThreshold = 0.05;
 	
 	public LogitechJoystick(int port) {
 		super(port);
 		for (int i = 0; i < 12; i++) {
 			buttons[i] = new SuperButton(this, i + 1); // Initialize all the buttons. Remember the index is one less than the button num
+		}
+	}
+	
+	public boolean active(int axis) {
+		if (axis == X_AXIS) {
+			return super.getX() > moveThreshold;
+		} else if (axis == Y_AXIS) {
+			return super.getY() > moveThreshold;
+		} else {
+			return false;
 		}
 	}
 }
