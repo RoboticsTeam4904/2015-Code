9d70ec342f6c727a4cad700d53e0eb2181c8268a
Carter Turnbaugh <carturn@nuevaschool.org>
2015-03-26 15:20:17 -0700

    Added positiving to NAVX class.

--- 2015-Code/src/org/usfirst/frc4904/robot/input/NAVX.java
+++ 2015-Code/src/org/usfirst/frc4904/robot/input/NAVX.java
@@ -8,4 +8,16 @@ public class NAVX extends AHRS {
 	public NAVX() {
 		super(new SerialPort(57600, SerialPort.Port.kMXP), (byte) 50);
 	}
+	
+	public float getYaw() {
+		return (super.getYaw() + 360) % 360;
+	}
+	
+	public float getPitch() {
+		return (super.getPitch() + 360) % 360;
+	}
+	
+	public float getRoll() {
+		return (super.getRoll() + 360) % 360;
+	}
 }
