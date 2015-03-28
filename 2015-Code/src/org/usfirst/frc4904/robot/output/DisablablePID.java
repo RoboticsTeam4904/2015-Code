500309d98e90ae3a050b356f9d4a764946e663f1
c0d3rman <yonilern@gmail.com>
2015-03-27 23:47:46 -0700

    Added comments to DisablablePID

--- 2015-Code/src/org/usfirst/frc4904/robot/output/DisablablePID.java
+++ 2015-Code/src/org/usfirst/frc4904/robot/output/DisablablePID.java
@@ -6,14 +6,32 @@ import edu.wpi.first.wpilibj.PIDOutput;
 import edu.wpi.first.wpilibj.PIDSource;
 
 public class DisablablePID extends PIDController {
-	private final boolean enabled;
+	private final boolean enabled; // This acts as an override for PIDController. Setting this to false will prevent the controller from activating or doing anything.
 	
+	/**
+	 * The DisablablePID class allows for code switches to kill PID.
+	 * The class has a one-off switch at construction time to kill the underlying PIDController for all eternity.
+	 * 
+	 * @param Kp
+	 *        P constant for PID
+	 * @param Ki
+	 *        I constant for PID
+	 * @param Kd
+	 *        D constant for PID
+	 * @param source
+	 *        Input source for PID loop
+	 * @param output
+	 *        Output device for PID loop
+	 * @param enabled
+	 *        Whether the PID controller is enabled or not
+	 */
 	public DisablablePID(double Kp, double Ki, double Kd, PIDSource source, PIDOutput output, boolean enabled) {
 		super(Kp, Ki, Kd, source, output);
 		this.enabled = enabled;
 	}
 	
 	public void enable() {
+		// Only enable PIDController if we are not disabled
 		if (enabled) {
 			super.enable();
 		}
