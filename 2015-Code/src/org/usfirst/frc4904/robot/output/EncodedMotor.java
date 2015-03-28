5fb871df4c1def329a7e9c6146a4411be1f5885c
c0d3rman <yonilern@gmail.com>
2015-03-27 23:48:04 -0700

    Brought back EncodedMotor

---
+++ 2015-Code/src/org/usfirst/frc4904/robot/output/EncodedMotor.java
@@ -0,0 +1,60 @@
+package org.usfirst.frc4904.robot.output;
+
+
+import org.usfirst.frc4904.robot.Disablable;
+import org.usfirst.frc4904.robot.Enablable;
+import org.usfirst.frc4904.robot.Overridable;
+import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.wpilibj.PIDOutput;
+import edu.wpi.first.wpilibj.Talon;
+
+public class EncodedMotor extends Talon implements Disablable, Enablable, Overridable<Double>, PIDOutput {
+	private final Encoder encoder;
+	private final DisablablePID pid;
+	private final static boolean ENABLED = false;
+	
+	public EncodedMotor(int channel, Encoder encoder, double Kp, double Ki, double Kd) {
+		super(channel);
+		this.encoder = encoder;
+		// Zero the encoder.
+		encoder.reset();
+		// Set the encoders to use rate for PID.
+		encoder.setPIDSourceParameter(Encoder.PIDSourceParameter.kRate);
+		// Initialize and configure the PID Controller.
+		pid = new DisablablePID(Kp, Ki, Kd, encoder, this, ENABLED);
+		pid.setOutputRange(-1, 1);
+		pid.setAbsoluteTolerance(0.1);
+		// Make sure the PID doesn't start out enabled
+		pid.disable();
+	}
+	
+	/**
+	 * Set the desired speed for the motor.
+	 * 
+	 * @param speed
+	 *        A double from -1 to 1 representing the desired speed
+	 */
+	public void set(double speed) {
+		pid.setSetpoint(speed);
+	}
+	
+	public void enable() {
+		pid.enable();
+		pid.setSetpoint(0);
+	}
+	
+	public void disable() {
+		set(0);
+		pid.setSetpoint(0);
+		pid.disable();
+	}
+	
+	public void override(Double speed) {
+		pid.disable();
+		super.set(speed);
+	}
+	
+	public void stopOverride() {
+		enable();
+	}
+}
