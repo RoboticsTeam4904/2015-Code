73f6774fc857213310d0fe06ec600a10cf1ac0b4
c0d3rman <yonilern@gmail.com>
2015-03-29 13:53:47 -0700

    Refactored NAVX to NavX

--- 2015-Code/src/org/usfirst/frc4904/robot/input/NAVX.java
+++ 2015-Code/src/org/usfirst/frc4904/robot/input/NAVX.java
@@ -4,8 +4,8 @@ package org.usfirst.frc4904.robot.input;
 import com.kauailabs.navx_mxp.AHRS;
 import edu.wpi.first.wpilibj.SerialPort;
 
-public class NAVX extends AHRS {
-	public NAVX() {
+public class NavX extends AHRS {
+	public NavX() {
 		super(new SerialPort(57600, SerialPort.Port.kUSB));
 		super.zeroYaw();
 	}
