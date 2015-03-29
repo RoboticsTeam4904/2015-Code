9ec1f272b049024d980978548da172cd64eadeb3
c0d3rman <yonilern@gmail.com>
2015-03-29 15:35:36 -0700

    Fixed formatting

--- 2015-Code/src/org/usfirst/frc4904/robot/input/NAVX.java
+++ 2015-Code/src/org/usfirst/frc4904/robot/input/NAVX.java
@@ -12,19 +12,28 @@ public class NavX extends AHRS {
 	
 	public float getYaw() {
 		float yaw = super.getYaw();
-		if (yaw < 0) return 360.00f + yaw;
-		else return yaw;
+		if (yaw < 0) {
+			return 360 + yaw;
+		} else {
+			return yaw;
+		}
 	}
 	
 	public float getPitch() {
 		float pitch = super.getPitch();
-		if (pitch < 0) return 360.00f + pitch;
-		else return pitch;
+		if (pitch < 0) {
+			return 360 + pitch;
+		} else {
+			return pitch;
+		}
 	}
 	
 	public float getRoll() {
 		float roll = super.getRoll();
-		if (roll < 0) return 360.00f + roll;
-		else return roll;
+		if (roll < 0) {
+			return 360 + roll;
+		} else {
+			return roll;
+		}
 	}
 }
